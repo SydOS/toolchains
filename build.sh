2a7cc32c0fea584ebefe84698369ca758aeb1188
Sydney Erickson <sydneyerickson@icloud.com>
2019-01-22 23:40:32 -0800

    Build various GCC and binutils toolchains

--- build.sh
+++ build.sh
@@ -15,9 +15,11 @@ export TOOLBASEPATH=/opt/SydOS-toolchain-base/
 export PATH=$TOOLPATH:$PATH
 
 set -e
-rm -rf build-* isl* cloog* gmp* mpfr* mpc* binutils* gcc*
+rm -rf build-* isl* cloog* gmp* mpfr* mpc* binutils* gcc* syods-toolchain-*.tar.gz
 sudo rm -rf $TOOLPATH $TOOLBASEPATH
 
+#-------------------------------------------------------------------------------
+# Toolchain Depenancies
 #-------------------------------------------------------------------------------
 
 wget $GNU_BASE/gmp/gmp-$GMP_VER.tar.xz
@@ -65,22 +67,42 @@ make -j4
 sudo make install
 cd ..
 
-wget $GNU_BASE/binutils/binutils-$BINUTILS_VER.tar.gz
-tar -xf binutils-$BINUTILS_VER.tar.gz
-mkdir build-binutils
-cd build-binutils
-../binutils-$BINUTILS_VER/configure --with-isl=$TOOLBASEPATH --with-cloog=$TOOLBASEPATH --prefix=$TOOLPATH --with-sysroot --disable-nls --disable-werror --target=$TARGET
-make -j4
-sudo make install
-cd ..
+tar -zcvf sydos-toolchain-base.tar.gz $TOOLBASEPATH
 
+#-------------------------------------------------------------------------------
+# GCC & Binutils
+#-------------------------------------------------------------------------------
+
+wget $GNU_BASE/binutils/binutils-$BINUTILS_VER.tar.gz
 wget $GNU_BASE/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.gz
-tar -xf gcc-$GCC_VER.tar.gz
-mkdir build-gcc
-cd build-gcc
-../gcc-$GCC_VER/configure --with-isl=$TOOLBASEPATH --with-cloog=$TOOLBASEPATH --with-gmp=$TOOLBASEPATH --with-mpfr=$TOOLBASEPATH --with-mpc=$TOOLBASEPATH --prefix=$TOOLPATH --disable-nls --enable-languages=c,c++ --without-headers --target=$TARGET
-make all-gcc -j4
-make all-target-libgcc -j4
-sudo make install-gcc
-sudo make install-target-libgcc
-cd ..
+
+function gcc_binutils {
+	tar -xf binutils-$BINUTILS_VER.tar.gz
+	mkdir build-binutils
+	cd build-binutils
+	../binutils-$BINUTILS_VER/configure --with-isl=$TOOLBASEPATH --with-cloog=$TOOLBASEPATH --prefix=$TOOLPATH --with-sysroot --disable-nls --disable-werror --target=$1
+	make -j4
+	sudo make install
+	cd ..
+
+	tar -xf gcc-$GCC_VER.tar.gz
+	mkdir build-gcc
+	cd build-gcc
+	../gcc-$GCC_VER/configure --with-isl=$TOOLBASEPATH --with-cloog=$TOOLBASEPATH --with-gmp=$TOOLBASEPATH --with-mpfr=$TOOLBASEPATH --with-mpc=$TOOLBASEPATH --prefix=$TOOLPATH --disable-nls --enable-languages=c,c++ --without-headers --target=$1
+	make all-gcc -j4
+	make all-target-libgcc -j4
+	sudo make install-gcc
+	sudo make install-target-libgcc
+	cd ..
+
+	tar -zcvf sydos-toolchain-$1.tar.gz $TOOLPATH
+	sudo rm -rf $TOOLPATH
+	rm -rf build-gcc build-binutils gcc-$GCC_VER binutils-$BINUTILS_VER
+}
+
+gcc_binutils x86_64-elf
+gcc_binutils i786-elf
+gcc_binutils i686-elf
+gcc_binutils i586-elf
+gcc_binutils i486-elf
+gcc_binutils i386-elf
